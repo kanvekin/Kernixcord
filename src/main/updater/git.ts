be39cab0f18ef80f3af829741bebce625fa80f83
kaanseas <126435851+kanvekin@users.noreply.github.com>
2026-08-09 05:06:02 +0300

    updater fix

--- src/main/updater/git.ts
+++ src/main/updater/git.ts
@@ -25,7 +25,7 @@ import { promisify } from "util";
 import { serializeErrors } from "./common";
 
 const VENCORD_SRC_DIR = join(__dirname, "..");
-const EQUICORD_DIR = join(__dirname, "../../");
+const KERNIXCORD_DIR = join(__dirname, "../../");
 
 const execFile = promisify(cpExecFile);
 
@@ -34,7 +34,7 @@ const isFlatpak = process.platform === "linux" && !!process.env.FLATPAK_ID;
 if (process.platform === "darwin") process.env.PATH = `/usr/local/bin:${process.env.PATH}`;
 
 function git(...args: string[]) {
-    const opts = { cwd: VENCORD_SRC_DIR };
+    const opts = { cwd: KERNIXCORD_DIR };
 
     if (isFlatpak) return execFile("flatpak-spawn", ["--host", "git", ...args], opts);
     else return execFile("git", args, opts);
@@ -73,7 +73,7 @@ async function pull() {
 }
 
 async function build() {
-    const opts = { cwd: EQUICORD_DIR };
+    const opts = { cwd: KERNIXCORD_DIR };
 
     const command = isFlatpak ? "flatpak-spawn" : "node";
     const args = isFlatpak ? ["--host", "node", "scripts/build/build.mjs"] : ["scripts/build/build.mjs"];
